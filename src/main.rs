b78ad38df5ac031e3479832443ac4f3210b5eb36
Joseph Yañez <chocheyg27@gmail.com>
2026-02-23 22:43:08 -0600

    Fix puerto para Render

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use actix_web::{App, HttpServer};
 use dotenv::dotenv;
 use std::env;
 use actix_files::Files;
+
 mod config;
 mod db;
 mod middleware;
 mod models;
 mod modules;
 mod services;
+
 use crate::middleware::jwt_middleware::JwtMiddleware;
 
 #[actix_web::main]
@@ -18,19 +20,24 @@ async fn main() -> std::io::Result<()> {
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL no definida");
 
-let pool: sqlx::PgPool =
-    db::connection::init_pool(&database_url).await;
+    let pool: sqlx::PgPool =
+        db::connection::init_pool(&database_url).await;
+
+    // 👇 IMPORTANTE PARA RENDER
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string());
+
+    println!("Servidor corriendo en puerto {}", port);
 
     HttpServer::new(move || {
         App::new()
-            
             .app_data(actix_web::web::Data::new(pool.clone()))
             .wrap(JwtMiddleware)
             .service(Files::new("/static", "./static").show_files_listing())
             .configure(modules::auth::init)
             .configure(modules::seguridad::init)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(format!("0.0.0.0:{}", port))?  // 👈 CAMBIO CLAVE
     .run()
     .await
 }
